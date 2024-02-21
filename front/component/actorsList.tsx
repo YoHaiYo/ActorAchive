eb8f8c5ead96fefdaabf10c223d5486f94672674
YoHaiYo <12si47bun@naver.com>
2024-02-21 11:22:04 +0900

    SPA 페이지네이션 구현

--- front/component/actorsList.tsx
+++ front/component/actorsList.tsx
@@ -8,31 +8,50 @@ import ActorsCast from './actorsCast';
 export default function ActorsList() {
   const [data, setData] = useState(null);
   const [page, setPage] = useState(1);
-  // Popular_Actor_API
+  const [inputPage, setInputPage] = useState('1'); // 사용자 입력 페이지 번호를 위한 state 추가
 
   useEffect(() => {
-    const Popular_Actor_API_page = `${Popular_Actor_API}&page=${page}`
+    const Popular_Actor_API_page = `${Popular_Actor_API}&page=${page}`;
     fetch(Popular_Actor_API_page)
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error("Fetching data failed", error));
-  }, [page]); // 페이지 바뀔때마다 마운트
+  }, [page]);
+
+  const goToNextPage = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const goToPreviousPage = () => {
+    setPage(prevPage => Math.max(1, prevPage - 1));
+  };
+
+  const handleInputChange = (e) => {
+    const newPage = Number(e.target.value);
+    setInputPage(e.target.value); // 입력값을 inputPage state에 저장
+
+    // 입력한 페이지 번호로 바로 이동
+    if (!isNaN(newPage) && newPage > 0) {
+      setPage(newPage);
+    }
+  };
 
   if (!data) return <div>Loading...</div>;
-  // console.log(data)
+
   return (
     <div>
       <h2>actors</h2>
-      <button onClick={() => { setPage(1) }}><Link href={`/actorlist/${page}`}>1</Link></button>
-      <button onClick={() => { setPage(2) }}><Link href={`/actorlist/${page}`}>2</Link></button>
-      <span>...</span>
-      <button onClick={() => { setPage(500) }}><Link href={`/actorlist/${page}`}>500</Link></button>
+      {page > 1 && <button onClick={goToPreviousPage}>Before</button>}
+      <input
+        type="number"
+        value={page}
+        onChange={handleInputChange}
+      />
+      <button onClick={goToNextPage}>Next</button>
 
-
-
-      {data.results.map((el, idx) => {
-        return (
-          <Link href={`/actor/${el.id}`} key={idx}>
+      {data.results.map((el, idx) => (
+        <Link href={`/actor/${el.id}`} key={idx}>
+          <div>
             <p>{el.id}</p>
             <p>{el.name}</p>
             <p>{el.popularity}</p>
@@ -41,9 +60,10 @@ export default function ActorsList() {
               actor_name={el.name}
               actor_popularity={el.popularity}
             />
-          </Link>
-        )
-      })}
-    </div >
+          </div>
+        </Link>
+      ))}
+    </div>
   );
 }
+
