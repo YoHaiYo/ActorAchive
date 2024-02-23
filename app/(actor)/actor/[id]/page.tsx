41a097c3b87586908049d4b4a74d92c78bc2b506
YoHaiYo <12si47bun@naver.com>
2024-02-23 16:40:30 +0900

    배우 세부페이지 스타일작업

--- app/(actor)/actor/[id]/page.tsx
+++ app/(actor)/actor/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Calendar2DateFill, Shop } from "react-bootstrap-icons";
 import { Actor_Cast_API_1, Actor_Cast_API_2, Actor_Detail_API_1, Actor_Detail_API_2, Poster_API } from "../../../../front/apiurl/apiurl";
 import ActorsCast from "../../../../front/component/actorsCast"
 import "./style/style.css"
@@ -8,6 +9,19 @@ export const metadata = {
 
 interface IPrams {
   params: { id: string }
+}
+
+export function ratingToStars(voteAverage) {
+  // 평점을 2로 나누어 0에서 5 사이의 값으로 조정
+  const starsFull = Math.round(voteAverage / 2);
+  const starsEmpty = 5 - starsFull; // 빈 별의 개수
+
+  // ★ 문자로 채워진 별과 ☆ 문자로 채워진 빈 별을 생성
+  const fullStars = '★'.repeat(starsFull);
+  const emptyStars = '☆'.repeat(starsEmpty);
+
+  // 채워진 별과 빈 별을 결합하여 최종 문자열 반환
+  return fullStars + emptyStars;
 }
 
 async function getCastDetail(id) {
@@ -28,27 +42,47 @@ export default async function Page(
   const castData = await getCastDetail(id);
   const actorData = await getActorDetail(id);
   return (
-    <section className="actor-detail">
+    <section className="actor-detail ">
       {/* <p>id : {id}</p> */}
-      <div className="d-flex">
+      <div className="d-lg-flex">
         <div className="actor-info">
-          <img src={`${Poster_API}/${actorData.profile_path}`} alt={actorData.name} style={{ width: 300 }} />
-          <p>{actorData.name}</p>
-          <p>Birth</p>
-          <p>{actorData.birthday}</p>
-          <p>Birth Place</p>
-          <p>{actorData.place_of_birth}</p>
+          <div className="actor-info-content">
+            <img src={`${Poster_API}/${actorData.profile_path}`} alt={actorData.name} style={{ width: 300 }} />
+            <p className="actor-info-name text-center">{actorData.name}</p>
+            <div className="actor-info-gray d-flex align-items-center">
+              <Calendar2DateFill className="me-2" />
+              <p className="">Birth</p>
+            </div>
+            <p>{actorData.birthday}</p>
+            <div className="actor-info-gray d-flex align-items-center">
+              <Shop className="me-2" />
+              <p className="">Birth Place</p>
+            </div>
+            <p>{actorData.place_of_birth}</p>
+          </div>
         </div>
         {/* <h3>출연작 (for cast)</h3> */}
-        <div className="d-flex flex-wrap">
+        <div className="d-flex flex-wrap justify-content-center">
           {castData.cast.map((el, idx) => {
             return (
-              <div className="actor-detail-movie" key={idx}>
-                <img src={`${Poster_API}/${el.backdrop_path}`} alt={el.name} />
+              <div className="actor-detail-moviecard" key={idx}>
+                <div className="d-flex justify-content-between mx-2 align-items-center">
+                  <p className="me-2">vote: {el.vote_count}</p>
+                  <div className="d-flex">
+                    <p className="maincolor me-1">{el.vote_average.toFixed(1)}</p>
+                    <p className="me-2 maincolor">
+                      {ratingToStars(el.vote_average)}
+                    </p>
+                  </div>
 
-                <p>{el.title}</p>
-                <p>vote_average : {el.vote_average}</p>
-                <p>vote_count : {el.vote_count}</p>
+                </div>
+                <div className="d-flex justify-content-center my-2">
+                  <img src={`${Poster_API}/${el.backdrop_path}`} alt={el.name} />
+                </div>
+
+                <div className="d-flex justify-content-center">
+                  <p className="me-2 movie-title">{el.title}</p>
+                </div>
               </div>
             )
           })}
